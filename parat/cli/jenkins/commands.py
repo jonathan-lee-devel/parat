50ec2b86b88185de5999ee55f6eb45abe9a4f61b
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-07-31 23:00:47 +0100

    Introduced constants

--- parat/cli/jenkins/commands.py
+++ parat/cli/jenkins/commands.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 from parat.cli.options import verbose_option, job_name_option, build_number_option, url_end_option, \
     build_jobs_yaml_file_option
+from parat.constants import BUILD, HOSTS, JENKINS_URL, JENKINS_USER, JENKINS_TOKEN
 from parat.use_cases.jenkins_builds import process_build_host
 from parat.utils.jenkins.jekins_request_settings import JenkinsRequestSettings
 from parat.utils.jenkins.jenkins_utils import get_jenkins_console_output, get_jenkins_job_dict, start_jenkins_build, \
@@ -28,7 +29,7 @@ def start_build(verbose: bool, job_name: str) -> None:
     initialize_logging(verbose)
     logging.info(f'Kicking off build ({job_name})...')
     response_status_code = start_jenkins_build(
-        JenkinsRequestSettings(os.getenv('JENKINS_URL'), (os.getenv('JENKINS_USER'), os.getenv('JENKINS_TOKEN')), 1),
+        JenkinsRequestSettings(os.getenv(JENKINS_URL), (os.getenv(JENKINS_USER), os.getenv(JENKINS_TOKEN)), 1),
         job_name).status_code
     if response_status_code == 201:
         logging.info(f'Successfully kicked off build ({job_name})!')
@@ -44,7 +45,7 @@ def start_build_url(verbose: bool, url_end: str) -> None:
     initialize_logging(verbose)
     logging.info(f'Kicking off build ({url_end})...')
     response_status_code = start_jenkins_build_url_end(
-        JenkinsRequestSettings(os.getenv('JENKINS_URL'), (os.getenv('JENKINS_USER'), os.getenv('JENKINS_TOKEN')), 1),
+        JenkinsRequestSettings(os.getenv(JENKINS_URL), (os.getenv(JENKINS_USER), os.getenv(JENKINS_TOKEN)), 1),
         url_end)
     if response_status_code == 201:
         logging.info(f'Successfully kicked off build ({url_end})!')
@@ -62,8 +63,8 @@ def start_build_jobs_yaml(verbose: bool, build_jobs_yaml: str) -> None:
     with open(build_jobs_yaml, 'r') as yaml_file_contents:
         build_jobs_dict = yaml.safe_load(yaml_file_contents)
         remaining_builds_dict = build_jobs_dict
-        for build_host_index in range(len(build_jobs_dict['build']['hosts'])):
-            build_host = build_jobs_dict['build']['hosts'][build_host_index]
+        for build_host_index in range(len(build_jobs_dict[BUILD][HOSTS])):
+            build_host = build_jobs_dict[BUILD][HOSTS][build_host_index]
             jobs_info_dict = process_build_host(build_host)
     logging_line_break()
     logging.info(f'Run of {build_jobs_yaml} completed:')
@@ -76,7 +77,7 @@ def start_build_jobs_yaml(verbose: bool, build_jobs_yaml: str) -> None:
         logging.error(f'Failed builds: {jobs_info_dict["failed_jobs"]}')
         delete_count = 0
         for successful_job in jobs_info_dict['successful_jobs']:
-            del remaining_builds_dict['build']['hosts'][build_host_index]['jobs'][successful_job['index'] - delete_count]
+            del remaining_builds_dict[BUILD][HOSTS][build_host_index]['jobs'][successful_job['index'] - delete_count]
             delete_count += 1
         output_file_name = build_jobs_yaml.replace('.yaml', '-remaining-output.yaml')
         logging.info(f'Outputting remaining (failed) jobs to {output_file_name}...')
@@ -93,7 +94,7 @@ def get_console_output(verbose: bool, job_name: str, build_number: int) -> None:
     initialize_logging(verbose)
     logging.info(f'Getting console output for ({job_name}) build number #{build_number}...')
     console_output = get_jenkins_console_output(
-        JenkinsRequestSettings(os.getenv('JENKINS_URL'), (os.getenv('JENKINS_USER'), os.getenv('JENKINS_TOKEN')), 1),
+        JenkinsRequestSettings(os.getenv(JENKINS_URL), (os.getenv(JENKINS_USER), os.getenv(JENKINS_TOKEN)), 1),
         job_name, build_number)
     logging.info(f'Console output: \n{console_output}')
 
@@ -107,6 +108,6 @@ def get_jenkins_json(verbose: bool, job_name: str, build_number: int) -> None:
     initialize_logging(verbose)
     logging.info(f'Getting API JSON for ({job_name}) build number #{build_number}...')
     jenkins_dict = get_jenkins_job_dict(
-        JenkinsRequestSettings(os.getenv('JENKINS_URL'), (os.getenv('JENKINS_USER'), os.getenv('JENKINS_TOKEN')), 1),
+        JenkinsRequestSettings(os.getenv(JENKINS_URL), (os.getenv(JENKINS_USER), os.getenv(JENKINS_TOKEN)), 1),
         job_name, build_number)
     logging.info(f'Jenkins JSON: \n{jenkins_dict}')
