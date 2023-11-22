3a2f76fc31122dd3ea5a928ad23b9eaf0ca266d9
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-11-22 20:15:46 +0000

    Removed references to release-train

--- parat/__main__.py
+++ parat/__main__.py
@@ -1,10 +1,9 @@
 import click
 
 from parat.cli.jenkins.commands import jenkins_commands
-from parat.cli.release_train.commands import release_train_commands
 
 # noinspection PyTypeChecker
-cli = click.CommandCollection(sources=[release_train_commands, jenkins_commands])
+cli = click.CommandCollection(sources=[jenkins_commands])
 
 if __name__ == '__main__':
     cli()
